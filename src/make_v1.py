dc7c90a543e0366fbef3120f39d942da3c1c6675
Martin Wellman <mwellman@ohri.ca>
2024-04-04 11:54:46 -0700

    Moved generated schemas/data to gen directory, renamed utils.py to general_utils.py, added check of __name__

--- src/make_v1.py
+++ src/make_v1.py
@@ -5,17 +5,18 @@ Make the ODMv1 LinkML schema.
 
 from pathlib import Path
 
-from utils import make_linkml_schema, get_logger
+from utils.general_utils import make_linkml_schema, get_logger
 
-# The directory to save the final odm_v1.yaml LinkML schema file to
-output_dir = Path("../odm_v1")
-# The directory where all the manually created Schemasheets are for ODM v1
-schemasheets_dir = output_dir / "schemasheets"
+if __name__ == "__main__":
+    # The directory to save the final odm_v1.yaml LinkML schema file to
+    output_dir = Path("../gen/odm_v1")
+    # The directory where all the manually created Schemasheets are for ODM v1
+    schemasheets_dir = output_dir / "schemasheets"
 
-logger = get_logger(__name__)
+    logger = get_logger(__name__)
 
-# Make the schema
-linkml_schema = output_dir / "linkml" / "odm_v1.yaml"
-make_linkml_schema(schemasheets_dir, linkml_schema)
+    # Make the schema
+    linkml_schema = output_dir / "linkml" / "odm_v1.yaml"
+    make_linkml_schema(schemasheets_dir, linkml_schema)
 
-logger.info("Finished!")
+    logger.info("Finished!")
